e36544f4927f58a7c050736fe0fe0622e6d3a9b2
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2025-01-05 16:12:26 +0200

    refactor(developer): Refactor role execution
    This commit refactors how roles are executed. It now uses `aichat` directly instead of relying
    on a custom script.
    
    The change was made to simplify the code and make it more maintainable. It also allows for
    better integration with the `aichat` tool.

--- agents/developer/tools.sh
+++ agents/developer/tools.sh
@@ -3,6 +3,8 @@
 # shellcheck disable=SC2154  # arguments are handled by argc
 set -e
 
+export AICHAT_ROLES_DIR="$HOME/.config/configent/prompts/assistant"
+
 # @cmd Get available roles and their documentation
 # @env LLM_OUTPUT=/dev/stdout The output path
 get_roles() {
@@ -14,11 +16,21 @@ get_roles() {
   cat "$HOME/.config/configent/prompts/developer.md" >>"$LLM_OUTPUT"
 }
 
-# @cmd Execute role
-# @option --role! Role
+# @cmd Executes the given role
+# @option --role! Role to execute
+# @option --task Task to execute
 execute_role() {
-  AICHAT_ROLES_DIR="$HOME/.config/configent/prompts/developer" \
-    aichat --role="$argc_role"
+  local context="${PWD##*/}"
+
+  # handle built-in aichat roles
+  case "$argc_role" in
+  code | create-prompt | explain-shell | shell)
+    argc_role="%$argc_role%"
+    printf "Matching built-in role %s\n\n" "$argc_role"
+    ;;
+  esac
+
+  aichat --role "$argc_role" --session "$context" "$argc_task"
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
